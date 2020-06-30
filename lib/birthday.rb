8f407c1f96951df7a3913ab8e677c767fef41f69
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-30 01:35:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,5 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.collect do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
   # add your code snippet here!
 end
